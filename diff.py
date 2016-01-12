7d0748e505e2762f93baf7f19239c483abd71371
Sw4mpf0x <Sw4mpf0x@users.noreply.github.com>
2016-01-12 12:18:58 -0600

    Updated. Actually works now.

--- diff.py
+++ diff.py
@@ -1,9 +1,13 @@
-#Show matched lines between two different files
-file1 = open("file1.txt", "r")
-file2 = open("file2.txt", "r")
+file1 = open("file2.txt", "r")
+file2 = open("file1.txt", "r")
 list1 = file1.readlines()
 list2 = file2.readlines()
+print "Unique in file2"
 for i in list2:
-    for j in list1:
-        if  i==j:
-            print(i)
+	if i not in list1:
+		print i
+
+print "Unique in file1"
+for i in list1:
+	if i not in list2:
+		print i
